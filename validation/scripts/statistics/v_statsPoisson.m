fd6b67ee647c1131d7149b200b98ce814a6701de
Brian Wandell <wandell@stanford.edu>
2017-10-11 16:15:56 -0700

    Made the plots look right.

--- validation/scripts/statistics/v_statsPoisson.m
+++ validation/scripts/statistics/v_statsPoisson.m
@@ -2,39 +2,50 @@
 %
 % Test the iePoisson random number generator calls
 %
+% Tests using poisspdf with the stats toolbox.
+%
 % BW ISETBIO Team, 2015
 
+%% 
 ieInit
 
 %% Create a small lambda set of samples and plot them
 
 % A small number
-nSamp = 1000;
-lambda = 1;
-v = iePoisson(lambda,nSamp);
-fprintf('Mean %.2f and variance %.2f\n',mean(v(:)), var(v));
+nSamp = [50000,1];
+lambda = 2;
+v = iePoisson(lambda,'nSamp',nSamp);
 
 vcNewGraphWin;
-hist(v,20);
+edges = 0:9;
+h = histogram(v(:),edges);
+P = poisspdf(edges,lambda);
+hold on; plot(edges + 0.5,P*nSamp(1),'-o');
 
 
 %% A slightly larger number
-nSamp = 1000;
-lambda = 25;
-v = iePoisson(lambda,nSamp);
-fprintf('Mean %.2f and variance %.2f\n',mean(v(:)), var(v));
+lambda = 8;
+v = iePoisson(lambda,'nSamp',nSamp);
+% fprintf('Mean %.2f and variance %.2f\n',mean(v(:)), var(v));
 
-vcNewGraphWin;
-hist(v,20);
+% vcNewGraphWin;
+edges = lambda + [-lambda:lambda];
+h = histogram(v(:),edges);
+P = poisspdf(edges,lambda);
+hold on; plot(edges + 0.5,P*nSamp(1),'-o');
 
 %%  Now try a big lambda, which should be gaussian
 
-lambda = 1000;
-v = iePoisson(lambda,nSamp);
+lambda = 16;
+v = iePoisson(lambda,'nSamp',nSamp);
 fprintf('Mean %.2f and variance %.2f\n',mean(v(:)), var(v));
 
-vcNewGraphWin;
-hist(v,20);
+edges = lambda + [-lambda:lambda];
+h = histogram(v(:),edges);
+P = poisspdf(edges,lambda);
+hold on; plot(edges + 0.5,P*nSamp(1),'-o');
 
+ylabel('Number of samples');
+xlabel('Sample value')
 
 %% END
